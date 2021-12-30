a8529f2bbe73df7a2357f817885896ffd302a37a
nilton <nfescame@gmail.comq:qclear>
2021-12-29 22:45:18 -0300

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const data = require("./data/project.json");
 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send(data);
 });
