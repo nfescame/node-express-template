b9d99386a59f4d168d16f8fde1afde8bb5a21048
nilton <nfescame@gmail.comq:qclear>
2021-12-29 21:58:49 -0300

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
- 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
- 
-app.listen(3000);
+
+app.listen(port, () => {
+  console.info(`Aplicação rodando em http://localhost:${port} `);
+});
